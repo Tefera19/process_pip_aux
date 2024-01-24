9b01a2c961c9fc043227eee8d5e81f3ab716bfa2
Tefera19 <tdegefu@worldbank.org>
2024-01-24 09:09:17 -0500

    edit the comments

--- harmonized_key_variables.R
+++ harmonized_key_variables.R
@@ -1,18 +1,17 @@
 library(data.table)
 library(dplyr)
 
-# Suggestion to harmonized auxiliary datasets key variable.
-# Purpose of this script is harmonized auxiliary data files
-# Here are the script for each of the auxiliary datasets that can be run to rename (if necessary) parts of the dataset’s key variables.
+# Suggestion to harmonized auxiliary data files key variables.
 
-# 1- reference year is going to renamed as surveyid_year
+# 1- reference year is going to be renamed as surveyid_year
 # 2- reporting level such us pop_data_level, cpi_data_level, pce_data_level, ppp_data_level, and gdp_data_level
 # will be renamed as reporting_level
 
+# Here are suggestions for each auxiliary to harmonize their key variables:
 
 # cpi ---------------------------------------------------
 # cpi - key variables consisted of country_code, cpi_year, survey_acronym, and cpi_data_level.
-# cpi_year and cpi_data_level variables is going to renamed as surveyid_year and reporting_level
+# cpi_year and cpi_data_level variables are going to be renamed as surveyid_year and reporting_level
 cpi <- pipload::pip_load_aux("cpi")
 cpi <- cpi |>
   setnames(c("cpi_year", "cpi_data_level"),
@@ -20,7 +19,7 @@ cpi <- cpi |>
 
 # pop ---------------------------------------------------
 # pop - key variables consisted of country_code, year, and pop_data_level
-# year and pop_data_level are going to renamed as surveyid_year and reporting_level
+# year and pop_data_level are going to be renamed as surveyid_year and reporting_level
 pop <- pipload::pip_load_aux("pop")
 pop <- pop |>
   setnames(c("year", "pop_data_level"),
@@ -28,7 +27,7 @@ pop <- pop |>
 
 # pce ---------------------------------------------------
 # pce - key variables consisted of country_code, year, and pce_data_level
-# year and pce_data_level are going to renamed as surveyid_year and reporting_level
+# year and pce_data_level are going to be renamed as surveyid_year and reporting_level
 pce <- pipload::pip_load_aux("pce")
 pce <- pce |>
   setnames(c("year", "pce_data_level"),
@@ -36,7 +35,7 @@ pce <- pce |>
 
 # gdp ---------------------------------------------------
 # gdp - key variables consisted of country_code, year, and gdp_data_level
-# year and gdp_data_level are going to renamed as surveyid_year and reporting_level
+# year and gdp_data_level are going to be renamed as surveyid_year and reporting_level
 gdp <- pipload::pip_load_aux("gdp")
 gdp <- gdp |>
   setnames(c("year", "gdp_data_level"),
@@ -44,7 +43,7 @@ gdp <- gdp |>
 
 # gdm ---------------------------------------------------
 # gdm key variables consisted of country_code, year, and pop_data_level
-# pop_data_level is going to renamed as reporting_level
+# pop_data_level is going to be renamed as reporting_level
 gdm <- pipload::pip_load_aux("gdm")
 gdm <- gdm |>
   setnames("pop_data_level", "reporting_level")
@@ -55,7 +54,7 @@ any(duplicated(gdm,
 
 # ppp ---------------------------------------------------
 # ppp - key variables consisted of country_code, ppp_year, ppp_data_level, ppp_data_level, release, adaption_version, and cpi_data_level.
-# ppp data needs to be filtered by ppp defualt variable and rename ppp_data_level to reporting_level
+# ppp data needs to be filtered by ppp defualt variable and then rename ppp_data_level to reporting_level
 # to merge it with any of the auxiliary datasets
 ppp <- pipload::pip_load_aux("ppp")
 ppp <- ppp[ppp_default == TRUE, ] |>
@@ -66,21 +65,21 @@ any(duplicated(ppp,
 
 # maddison ---------------------------------------------------
 # maddison - key variables consisted of country_code, and year.
-# year is going to renamed as surveyid_year
+# year is going to be renamed as surveyid_year
 maddison <- pipload::pip_load_aux("maddison")
 maddison <- maddison |>
   setnames("year", "surveyid_year")
 
 # weo ---------------------------------------------------
 # weo - key variables consisted of country_code, and year.
-# year is going to renamed as surveyid_year
+# year is going to be renamed as surveyid_year
 weo <- pipload::pip_load_aux("weo")
 weo <- weo |>
   setnames("year", "surveyid_year")
 
 # npl ---------------------------------------------------
 # npl - key variables consisted of country_code, and reporting_year.
-# reporting_year is going to renamed as surveyid_year * if content of the reporting_year
+# reporting_year is going to be renamed as surveyid_year * if content of the reporting_year
 # is similar to surveyid_year
 npl <- pipload::pip_load_aux("npl")
 npl <- npl |>
@@ -89,7 +88,7 @@ npl <- npl |>
 # pfw ---------------------------------------------------
 # pfw - key variables consisted of country_code, year [surveyid_year][survey_year][reporting_year],
 # and welfare_type [survey_acronym]. We need to get reporting_level variable to merge
-# pfw data with other auxiliary datasets. We need to generate reporting level from cpi dataset as follows:
+# pfw data with any of the other auxiliary datasets. We need to generate reporting level variable using cpi dataset as follows:
 
 #### ????? is possible to include reporting_level variable in the pfw? If possible we don't
 # need to run the following script that would generate reporting_level.
@@ -150,7 +149,7 @@ pfw <-  pfw_cpi_key[pfw, on= .(country_code, survey_year,
 
 # income_groups ---------------------------------------------------
 # income_groups - key variables consisted of country_code and year_data.
-# year_data is going to renamed as surveyid_year
+# year_data is going to be renamed as surveyid_year
 income_groups <- pipload::pip_load_aux("income_groups")
 income_groups <- income_groups |>
   setnames("year_data", "surveyid_year")
